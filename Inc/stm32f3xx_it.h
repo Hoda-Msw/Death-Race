1d665dd7ac0ccef77cf204b4f3ae3195b006b056
hoda <H.Moosavi1996@gmail.com>
2018-12-28 01:12:49 +0330

    Add files via upload

--- Inc/stm32f3xx_it.h
+++ Inc/stm32f3xx_it.h
@@ -56,7 +56,9 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+void EXTI0_IRQHandler(void);
 void TIM6_DAC_IRQHandler(void);
+void TIM7_IRQHandler(void);
 
 #ifdef __cplusplus
 }
